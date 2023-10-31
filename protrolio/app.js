708ade364d9d8f19a7b6bbbce51fe40350f8c94d
rahulvir <rahulmsd99999@gmail.com>
2023-10-31 14:18:40 +0530

    setup new Protfolio dirsitoty

--- protrolio/app.js
+++ protrolio/app.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const app = express();
+const port = 4000;
 const userModel = require("./database");
+
 app.set("view engine","ejs");
 
 app.get('/', (req, res) => {
